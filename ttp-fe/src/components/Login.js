86d80158b5d16f60e0934236c754f3c9cc53c314
Elias <eliasiscoding@gmail.com>
2019-06-30 21:08:06 -0400

    added the signup component now

--- ttp-fe/src/components/Login.js
+++ ttp-fe/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class Login extends Component {
@@ -34,4 +35,4 @@ class Login extends Component {
   }
 
 }
-export default Login
+export default connect(null,{})(Login)
--- ttp-fe/src/components/Signup.js
+++ ttp-fe/src/components/Signup.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
-class Login extends Component {
+class Signup extends Component {
+
+  state = {
+    name: "",
+    email: "",
+    password: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  }
+
   render() {
-    return()
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <h1> Signup </h1>
+        <label> Name
+          <input placeholder="Your Name" name="name" onChange={this.handleChange}/>
+        </label>
+        <label> Email
+          <input placeholder="Email" name="email" onChange={this.handleChange}/>
+         </label>
+        <label> Password
+          <input placeholder="Password" name="password" onChange={this.handleChange}/>
+        </label>
+        <button type="submit"> Submit </button>
+      </form>
+    )
   }
 
 }
-export default Login
+export default connect(null,{})(Signup)
