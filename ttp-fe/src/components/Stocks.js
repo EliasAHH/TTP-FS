e0c40c4ba58b3e180584485b948c520f56fface2
Elias <eliasiscoding@gmail.com>
2019-07-09 08:07:18 -0400

    testing something out

--- ttp-fe/src/components/Stocks.js
+++ ttp-fe/src/components/Stocks.js
@@ -5,15 +5,14 @@ const Stocks = props => {
   const { latestPrice, open, shares, symbol } = props.stock
 
   const checkOpen = () => {
-    if(latestPrice > open){
-      return <span> green </span>
-    }else if(latestPrice === open) {
-      return <span> grey </span>
-    }else {
-      return <span> red </span>
+      if(latestPrice > open){
+        return <span> green </span>
+      }else if(latestPrice === open) {
+        return <span> grey </span>
+      }else {
+        return <span> red </span>
     }
   }
-
     return (
       <div>
       {symbol} {shares} {latestPrice * shares} {checkOpen()}
